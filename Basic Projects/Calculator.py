edaa19b7c309cdf554f21f3cbb8985ed3bb830c7
Abdirahman Yasin <78784947+abdirahman-yy@users.noreply.github.com>
2023-08-08 21:44:17 -0700

    Update Calculator.py
    new updates -- more calculations

--- Basic Projects/Calculator.py
+++ Basic Projects/Calculator.py
@@ -6,7 +6,19 @@ def calc():
         ops = {
             1: "+", 2: "-", 3: "*", 4: "/",
             5: "square", 6: "cube", 7: "square root",
-            8: "derivative", 9: "integral"
+            8: "derivative", 9: "integral",
+            10: "sin", 11: "cos", 12: "tan",
+            13: "arcsin", 14: "arccos", 15: "arctan",
+            16: "log", 17: "exp",
+            18: "factorial", 19: "binomial",
+            20: "mean", 21: "median", 22: "mode",
+            23: "standard deviation", 24: "variance",
+            25: "lcm", 26: "gcd", 27: "abs",
+            28: "floor", 29: "ceil",
+            30: "matrix determinant", 31: "matrix inverse",
+            32: "matrix transpose", 33: "vector dot product",
+            34: "vector cross product", 35: "matrix eigenvalues",
+            36: "matrix rank", 37: "matrix trace",
         }
         print("Choose an operation:\n" + "\n".join([f"{key} - {value}" for key, value in ops.items()]))
         op = int(input())
@@ -30,6 +42,111 @@ def calc():
                 x = sp.symbols('x')
                 expr = n * x
                 result = sp.integrate(expr, x)
+            elif op in [10, 11, 12, 13, 14, 15]:
+                angle = n if op in [10, 11, 12] else sp.rad(n)
+                result = (
+                    sp.sin(angle) if op == 10 else
+                    sp.cos(angle) if op == 11 else
+                    sp.tan(angle) if op == 12 else
+                    sp.asin(angle) if op == 13 else
+                    sp.acos(angle) if op == 14 else
+                    sp.atan(angle)
+                )
+            elif op in [16, 17]:
+                result = sp.log(n) if op == 16 else sp.exp(n)
+            elif op == 18:
+                result = sp.factorial(n)
+            elif op == 19:
+                k = int(input("Enter the value of 'k' for the binomial coefficient: "))
+                result = sp.binomial(n, k)
+            elif op in [20, 21, 22]:
+                data = input("Enter a list of numbers separated by spaces: ").split()
+                data = [float(item) for item in data]
+                result = (
+                    sp.mean(data) if op == 20 else
+                    sp.median(data) if op == 21 else
+                    sp.mode(data) if op == 22 else
+                    0
+                )
+            elif op in [23, 24]:
+                data = input("Enter a list of numbers separated by spaces: ").split()
+                data = [float(item) for item in data]
+                mean = sp.mean(data)
+                result = (
+                    sp.sqrt(sp.mean([(x - mean)**2 for x in data])) if op == 23 else
+                    sp.mean([(x - mean)**2 for x in data]) if op == 24 else
+                    0
+                )
+            elif op == 25:
+                m = float(input("Enter another number: "))
+                result = sp.lcm(n, m)
+            elif op == 26:
+                m = float(input("Enter another number: "))
+                result = sp.gcd(n, m)
+            elif op in [27, 28, 29]:
+                result = (
+                    sp.Abs(n) if op == 27 else
+                    sp.floor(n) if op == 28 else
+                    sp.ceiling(n)
+                )
+            elif op == 30:
+                matrix_rows = int(input("Enter the number of rows: "))
+                matrix_cols = int(input("Enter the number of columns: "))
+                matrix_data = input("Enter matrix elements separated by spaces:\n").split()
+                matrix_data = [float(item) for item in matrix_data]
+                matrix = sp.Matrix(matrix_data).reshape(matrix_rows, matrix_cols)
+                result = matrix.det()
+            elif op == 31:
+                matrix_rows = int(input("Enter the number of rows: "))
+                matrix_cols = int(input("Enter the number of columns: "))
+                matrix_data = input("Enter matrix elements separated by spaces:\n").split()
+                matrix_data = [float(item) for item in matrix_data]
+                matrix = sp.Matrix(matrix_data).reshape(matrix_rows, matrix_cols)
+                result = matrix.inv()
+            elif op == 32:
+                matrix_rows = int(input("Enter the number of rows: "))
+                matrix_cols = int(input("Enter the number of columns: "))
+                matrix_data = input("Enter matrix elements separated by spaces:\n").split()
+                matrix_data = [float(item) for item in matrix_data]
+                matrix = sp.Matrix(matrix_data).reshape(matrix_rows, matrix_cols)
+                result = matrix.T
+            elif op == 33:
+                vector1_data = input("Enter vector 1 elements separated by spaces:\n").split()
+                vector1_data = [float(item) for item in vector1_data]
+                vector2_data = input("Enter vector 2 elements separated by spaces:\n").split()
+                vector2_data = [float(item) for item in vector2_data]
+                vector1 = sp.Matrix(vector1_data)
+                vector2 = sp.Matrix(vector2_data)
+                result = vector1.dot(vector2)
+            elif op == 34:
+                vector1_data = input("Enter vector 1 elements separated by spaces:\n").split()
+                vector1_data = [float(item) for item in vector1_data]
+                vector2_data = input("Enter vector 2 elements separated by spaces:\n").split()
+                vector2_data = [float(item) for item in vector2_data]
+                vector1 = sp.Matrix(vector1_data)
+                vector2 = sp.Matrix(vector2_data)
+                result = vector1.cross(vector2)
+            elif op == 35:
+                matrix_rows = int(input("Enter the number of rows: "))
+                matrix_cols = int(input("Enter the number of columns: "))
+                matrix_data = input("Enter matrix elements separated by spaces:\n").split()
+                matrix_data = [float(item) for item in matrix_data]
+                matrix = sp.Matrix(matrix_data).reshape(matrix_rows, matrix_cols)
+                result = matrix.eigenvals()
+            elif op == 36:
+                matrix_rows = int(input("Enter the number of rows: "))
+                matrix_cols = int(input("Enter the number of columns: "))
+                matrix_data = input("Enter matrix elements separated by spaces:\n").split()
+                matrix_data = [float(item) for item in matrix_data]
+                matrix = sp.Matrix(matrix_data).reshape(matrix_rows, matrix_cols)
+                result = matrix.rank()
+            elif op == 37:
+                matrix_rows = int(input("Enter the number of rows: "))
+                matrix_cols = int(input("Enter the number of columns: "))
+                matrix_data = input("Enter matrix elements separated by spaces:\n").split()
+                matrix_data = [float(item) for item in matrix_data]
+                matrix = sp.Matrix(matrix_data).reshape(matrix_rows, matrix_cols)
+                result = matrix.trace()
                 
             print(f"Result: {n} {ops[op]} {user_input if op in [1, 2, 3, 4] else ''} = {result}")
             
